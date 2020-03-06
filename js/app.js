a6acd40e19be578cde8848811a8fd5121f3755b1
Tommalieh <muhammedtommalieh@gmail.com>
2020-03-07 00:13:03 +0200

    Added services

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+
+
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(m) {
+  showSlides2(slideIndex2 += m);
+}
+
+// Thumbnail image controls
+function currentSlide2(m) {
+  showSlides2(slideIndex2 = m);
+}
+
+function showSlides2(m) {
+  var j;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  if (m > slides2.length) {slideIndex2 = 1}
+  if (m < 1) {slideIndex2 = slides2.length}
+  for (j = 0; j < slides2.length; j++) {
+      slides2[j].style.display = "none";
+  }
+  slides2[slideIndex2 - 1].style.display = "block";
+}
