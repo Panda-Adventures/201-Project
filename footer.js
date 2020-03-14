d1f938e66356fd4dcdcd8f3fbfd5bd55c6ca239a
Tommalieh <muhammedtommalieh@gmail.com>
2020-03-14 16:40:54 +0200

    Finished Footer

---
+++ footer.js
@@ -0,0 +1,35 @@
+// var defaultbtn = document.getElementsByClassName('btn1')
+// defaultbtn.addEventListener('click', function(e){
+//     e.preventDefault();
+// });
+
+// var defaultbtn = document.getElementById('btn1')
+// defaultbtn.addEventListener('click', function(e){
+//     e.preventDefault();
+// });
+
+$(function(){
+    $('.one form .btn').on('click',function(){ 
+       $(this).parents('.one').animate({
+            top : '-500px'
+          },500);
+      
+                                            $(this).parents('.one').siblings('.two').
+       animate({
+            top : '0px'
+          },500);
+      return false;
+    });
+  
+  $('.two .close').on('click',function(){
+    $(this).parent().animate({
+     top : '-500px'
+    },500);
+    
+    $(this).parent().siblings('.one').animate({
+     top : '0px'
+    },500);
+    return false;
+   });
+  });
+
